880bc417281b018d80b2f6a34a1e18ddc8ba0023
Abdullah <abdullahmasoodarif@gmail.com>
2022-10-20 02:18:19 +0500

    Added Product Create Delete and Upsert You can add delete and update the Product Functionality of Category and Brands Are already been done Next Step: Work on UI Interface

--- CraftBrothers/Controllers/ProductController.cs
+++ CraftBrothers/Controllers/ProductController.cs
@@ -1,16 +1,20 @@
 ﻿using CraftBrothers.Data;
 using CraftBrothers.Models;
+using CraftBrothers.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace CraftBrothers.Controllers
 {
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public ProductController(ApplicationDbContext db)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(ApplicationDbContext db,IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -22,39 +26,169 @@ namespace CraftBrothers.Controllers
             };
             return View(objList);
         }
+        [HttpGet]
         public IActionResult Upsert(int? id)
         {
-            IEnumerable<SelectListItem> CategoryDropdown = _db.Categories.Select(i => new SelectListItem
+
+            //IEnumerable<SelectListItem> CategoryDropdown = _db.Categories.Select(i => new SelectListItem
+            //{
+            //    Text = i.Name,
+            //    Value = i.Id.ToString()
+            //});
+            //ViewBag.CategoryDropdown = CategoryDropdown;
+
+
+            //IEnumerable<SelectListItem> BrandDropdown = _db.Brands.Select(i => new SelectListItem
+            //{
+            //    Text = i.Name,
+            //    Value = i.Id.ToString()
+            //});
+            //ViewBag.BrandDropdown = BrandDropdown;
+
+
+            //Product product = new Product();
+            ProductVM productVM = new ProductVM()
             {
-                Text = i.Name,
-                Value = i.Id.ToString()
-            });
-            ViewBag.CategoryDropdown = CategoryDropdown;
+                Product = new Product(),
+                CategorySelectList = _db.Categories.Select(i => new SelectListItem
+                {
+
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                BrandSelectList = _db.Brands.Select(i => new SelectListItem
+                {
+
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
 
 
-            IEnumerable<SelectListItem> BrandDropdown = _db.Brands.Select(i => new SelectListItem
-            {
-                Text = i.Name,
-                Value = i.Id.ToString()
-            });
-            ViewBag.BrandDropdown = BrandDropdown;
-
-
-            Product product = new Product();
+            };
+             
             if (id == null)
             {
-                return View(product);
+                return View(productVM);
             }
             else
             {
-                product = _db.Products.Find(id);
-                if(product == null)
+                productVM.Product = _db.Products.Find(id.GetValueOrDefault());
+                if(productVM.Product == null)
                 {
                     return NotFound();
                 }
-                return View(product);
+                return View(productVM);
 
             }
         }
+        // Post upsert
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM productVM)
+        {
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnvironment.WebRootPath;
+
+                if (productVM.Product.BrandId == 0)
+                {
+                    //Creating
+                    string upload = webRootPath + WC.ImagePath;
+                    string fileName = Guid.NewGuid().ToString();
+                    string extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+                    productVM.Product.Image = fileName + extension;
+                    _db.Products.Add(productVM.Product);
+                }
+
+                else
+                {
+                    //updating
+                    var objFromDb = _db.Products.AsNoTracking().FirstOrDefault(u => u.Name == productVM.Product.Name);
+                    if (files.Count() > 0)
+                    {
+                        string upload = webRootPath + WC.ImagePath;
+                        string fileName = Guid.NewGuid().ToString();
+                        string extension = Path.GetExtension(files[0].FileName);
+
+                        var oldFile = Path.Combine(upload, objFromDb.Image);
+                        if (System.IO.File.Exists(oldFile))
+                        {
+                            System.IO.File.Delete(oldFile);
+                        }
+                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+                        //new image 
+                        productVM.Product.Image = fileName + extension;
+                    }
+                    else
+                    {
+                        //keep image as it is 
+                        //other changes
+                        productVM.Product.Image = objFromDb.Image;
+                    }
+                    _db.Products.Update(productVM.Product);
+                }
+                 _db.SaveChanges();
+                return RedirectToAction("Index");
+            
+        }
+
+        //Get-delte
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product product = _db.Products.Include(u => u.Category).FirstOrDefault(u => u.Id == id);
+            
+            //Product product = _db.Products.FirstOrDefault(u => u.Id == id, includeProperties: "Category,Brand");
+            //product.Category = _db.Category.Find(product.CategoryId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        //POST - DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Products.Find(id.GetValueOrDefault());
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            string upload = _webHostEnvironment.WebRootPath + WC.ImagePath;
+            var oldFile = Path.Combine(upload, obj.Image);
+
+            if (System.IO.File.Exists(oldFile))
+            {
+                System.IO.File.Delete(oldFile);
+            }
+
+
+            _db.Remove(obj);
+            _db.SaveChanges();
+            //            TempData[WC.Success] = "Action completed successfully";
+            return RedirectToAction("Index");
+
+
+        }
+
     }
+
 }
+
---
+++ CraftBrothers/Models/ViewModels/ProductVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CraftBrothers.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public  Product Product { get; set; }
+        public  IEnumerable<SelectListItem> CategorySelectList { get; set; }
+        public  IEnumerable<SelectListItem> BrandSelectList { get; set; }
+    }
+}
